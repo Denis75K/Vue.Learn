51c26b072b18064606e47c09a2dd8a833e5a132b
Денис75K <knnagrina36@mail.ru>
2020-12-02 15:33:52 +0300

    02.12

---
+++ Vue.Learn/Vue.Learn/Controllers/WeatherForecastController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Vue.Learn.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        public class Product
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public int Quantity { get; set; }
+            public bool InStock { get; set; }
+        
+    }
+
+        [HttpGet("products")]
+        public IEnumerable<Product> GetProducts()
+        {
+            return new List<Product>()
+            {
+                new Product{Id=1,Name = "Car",Description = "Volvo",Quantity = 11,InStock = true},
+                new Product{Id=2,Name = "Phone",Description = "Moto Z2",Quantity = 5,InStock = false},
+            }.ToArray();
+        }
+    }
+}
